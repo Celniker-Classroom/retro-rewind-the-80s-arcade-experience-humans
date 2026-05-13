d3f11895ffaec9ba43df6e9710ef20345a361236
Tymur Chernykov <127988729+stealhead1@users.noreply.github.com>
2026-05-13 20:57:39 +0000

    made bullets work but not them being kinematic

--- sketch.js
+++ sketch.js
@@ -1,19 +1,29 @@
 let playerSpeed = 5;
 let playerImg;
 let player;
+let bullet;
+let shootDelay = 0;
 q5.preload = function() {
 	playerImg = loadImage("images/greenjet.png");
 }
 
 q5.setup = async function() {
 	await Canvas();
+	frameRate(60);
 	player = new Sprite();
-	player.w = 30;
+	player.w = 80;
 	player.h = 75;
 	player.image = playerImg;
+	player.image.scale = 3;
+	player.debug = true;
+	player.rotationLock = true;
+	bullet = new Group();
+	bullet.w = 10;
+	bullet.h = 30;
+	bullet.collider = 'k';
 }
 
-function playerMovement() {
+function playerControls() {
 	player.velocity.x = 0;
 	player.velocity.y = 0;
 	if (kb.pressing('w')) {
@@ -28,9 +38,18 @@ function playerMovement() {
 	else if (kb.pressing("d")) {
 		player.velocity.x = 1 * playerSpeed;
 	}
+	if (kb.pressing("space") && shootDelay < 1){
+		let shotBullet = new bullet.Sprite();
+		shotBullet.velocity.y = -5;
+		shotBullet.x = player.x;
+		shotBullet.y = player.y - player.height/2;
+		shootDelay = 30;
+		shotBullet.collider = 'k';
+	}
 }
 
 q5.draw = function () {
 	background('skyblue');
-	playerMovement();
+	playerControls();
+	shootDelay --;
 };
